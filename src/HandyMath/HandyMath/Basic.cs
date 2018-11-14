298e6f4f68b305c9351ccf3de6a174a62553a1c8
Jon Hutchings <jon.hutchings@derivco.co.za>
2018-11-14 12:37:43 +0200

    Add docs

--- src/HandyMath/HandyMath/Basic.cs
+++ src/HandyMath/HandyMath/Basic.cs
@@ -1,9 +1,17 @@
-﻿using System;
-
-namespace HandyMath
+﻿namespace HandyMath
 {
+  /// <summary>
+  /// Basic operations one might do on simple numeric types like integers.
+  /// </summary>
   public static class Basic
+
   {
+    /// <summary>
+    /// Add the supplied numbers.
+    /// </summary>
+    /// <param name="left">The left argument.</param>
+    /// <param name="right">The right argument.</param>
+    /// <returns>The sum of the supplied numbers.</returns>
     public static long Add(long left, long right)
     {
       return left + right;
