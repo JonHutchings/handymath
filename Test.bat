af799f81393db9288ab7f0d149684dd54d493ffa
Jon Hutchings <jon.hutchings@derivco.co.za>
2018-11-26 15:39:05 +0200

    Disable debug, enable /s

--- Test.bat
+++ Test.bat
@@ -2,12 +2,9 @@
 echo Hello World from %AGENT_NAME%.
 echo My ID is %AGENT_ID%.
 echo AGENT_WORKFOLDER contents:
-@dir %AGENT_WORKFOLDER%
+@dir %AGENT_WORKFOLDER% /s
 echo AGENT_BUILDDIRECTORY contents:
-@dir %AGENT_BUILDDIRECTORY%
+@dir %AGENT_BUILDDIRECTORY% /s
 echo BUILD_SOURCESDIRECTORY contents:
-@dir %BUILD_SOURCESDIRECTORY%
-
-echo And now for the big cheese...
 @dir %BUILD_SOURCESDIRECTORY% /s
 echo Over and out.
